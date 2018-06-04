e7bc2c88c639857a7181f776cf9397c6b7707346
hapiecoder <prashantpratapsingh021@gmail.com>
2018-06-04 15:43:40 +0530

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,27 @@ import { Vibration } from '@ionic-native/vibration';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { Camera } from '@ionic-native/camera';
-import { Flashlight } from '@ionic-native/flashlight';
+import { Backlight } from '@ionic-native/backlight';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { FileChooser } from '@ionic-native/file-chooser';
+import { File } from '@ionic-native/file';
+import firebase from 'firebase';
+import { DataProvider } from '../providers/data/data';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
+import { CallNumber } from '@ionic-native/call-number';
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
+import { BatteryStatus } from '@ionic-native/battery-status';
+
+let config={
+  apiKey: "AIzaSyBI_MvVhO5o3Z_vgBeyDxarTb9bS1KJ4jc",
+  authDomain: "sample-f1534.firebaseapp.com",
+  databaseURL: "https://sample-f1534.firebaseio.com",
+  projectId: "sample-f1534",
+  storageBucket: "",
+  messagingSenderId: "966190612602"
+};
+firebase.initializeApp(config);
+
 @NgModule({
   declarations: [
     MyApp,
@@ -16,6 +35,7 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
   ],
   imports: [
     BrowserModule,
+   
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -26,11 +46,18 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
   providers: [
     StatusBar,
     SplashScreen,
-    Flashlight,
+    Backlight,
     Vibration,
+    CallNumber,
     BarcodeScanner,
+    Contacts,
+    File,
+    FileChooser,
     Camera,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    BatteryStatus,
+    FingerprintAIO,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DataProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,40 +1,61 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Vibration } from '@ionic-native/vibration';
-
-import { Flashlight } from '@ionic-native/flashlight';
+import { FileChooser } from '@ionic-native/file-chooser';
+import { Backlight } from '@ionic-native/backlight';
 import { BarcodeScanner,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { Platform } from 'ionic-angular';
-
+import { File } from '@ionic-native/file';
+import firebase from 'firebase';
+import { FingerprintAIO,FingerprintOptions } from '@ionic-native/fingerprint-aio';
+import { CallNumber } from '@ionic-native/call-number';
+import { Contacts, Contact, ContactField, ContactName, ContactFieldType } from '@ionic-native/contacts';
+import { BatteryStatus } from '@ionic-native/battery-status';
+declare var cordova;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
+  
 })
+
 export class HomePage {
+  stat;
+  fingerprintOptions:FingerprintOptions;
   options: BarcodeScannerOptions;
 encodText:string="";
 encodedData:any={};
 scannedData:any={};
+backlightStatus:boolean=false;
+phoneNumber: number;
+wheretosearch: ContactFieldType[] = ["displayName"];
+q='';
+contactFound =[];
 
-  constructor(public navCtrl: NavController,private vibration: Vibration,private flashlight: Flashlight
-    ,private camera: Camera,private scanner: BarcodeScanner) {
-
-  }
+  
+  constructor(public navCtrl: NavController,private vibration: Vibration,private backlight: Backlight
+    ,private camera: Camera,private scanner: BarcodeScanner,private fileChooser: FileChooser,private file: File,
+  private platform : Platform,private faio: FingerprintAIO,private call: CallNumber,
+  private contacts: Contacts,private batteryStatus: BatteryStatus) {
+    this.search('');
+    this.getStatus();
+    }
      
   startvibration(){
       
     this.vibration.vibrate([2000,1000,2000]);
   }
   
-  startflash(){
-    
-    this.flashlight.switchOn();
-    console.log(this);
-   
+  on(){
+    this.backlight.on().then(() =>{
+      this.backlightStatus =true;
+     })
+  
   }
-  stopflash(){
-    this.flashlight.switchOff();
+  off(){
+    this.backlight.off().then(() =>{
+      this.backlightStatus =false;
+     })
   }
   cameraon(){
     const options: CameraOptions = {
@@ -69,7 +90,85 @@ scannedData:any={};
     console.log("Error :", err);
     })
     }
-    }
-    
-   
+    choose(){
+       // Check If Cordova/Mobile
+//   if (this.platform.is('cordova')) {
+   // window.location.href = "www.youtube.com";
+   //alert("is available")
+//}else{
+   // window.open("www.youtube.com",'_blank');
+  // alert("'is not available")
+//}
 
+      this.fileChooser.open().then((uri)=>{
+       alert(uri);
+       this.file.resolveLocalFilesystemUrl(uri).then((newUrl)=>{
+       alert(JSON.stringify(newUrl));
+      
+      let dirPath = newUrl.nativeURL;
+      let dirPathSegments = dirPath.split('/')
+      dirPathSegments.pop()
+      dirPath = dirPathSegments.join('/')
+      
+      this.file.readAsArrayBuffer(dirPath, newUrl.name).then(async(buffer)=>{
+      await this.upload(buffer,newUrl.name); //buffer is content of file in fbase
+      })
+      })
+      })
+      }
+      async upload(buffer,name){
+      let blob = new Blob([buffer], {type:"image/jpeg"});
+      
+      let storage= firebase.storage();
+      
+      storage.ref('image/' + name).put(blob).then((d)=>{
+      alert("done");
+      }).catch((error)=>{
+      alert(JSON.stringify(error))
+      })
+      } 
+      
+    
+    fingerprintdialoge(){
+      this.faio.show({
+        clientId: 'Fingerprint-demo',
+        clientSecret: 'password'
+        })
+        .then(result => {
+         this.navCtrl.setRoot('HomePage');
+        
+        })
+        .catch(err => {
+        console.log('Err:',err);
+        })
+    }
+    async callNumber():Promise<any>{
+      try{
+       await this.call.callNumber(String(this.phoneNumber), true);
+     }
+     catch(e){
+       console.error(e);
+     }
+     }
+     search(val) {
+       this.contacts.find(this.wheretosearch,{filter:this.q}).then((contacts) =>{
+         this.contactFound = contacts;
+       }).catch((err) =>{
+          alert(JSON.stringify(err));
+       })
+       }
+       ringerstatus(){
+        cordova.plugins.ringerMode.getRingerMode(function(ringerMode) {
+          console.log("The current ringerMode is:" + ringerMode);
+    });
+       }
+       getStatus(){
+        this.batteryStatus.onChange().subscribe(status=> {
+          this.stat = status;
+      });
+      }
+     }
+  
+
+// run command:
+//  ionic cordova run android   
